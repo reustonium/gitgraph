b5d17c97d2437ce01c3d0a6ff5e54047e1e2c6dd
reustonium <andrew.ruestow@gmail.com>
2015-11-07 19:59:58 -0500

    Convert STDOUT to string

--- gitgraph/graph.py
+++ gitgraph/graph.py
@@ -1,7 +1,12 @@
-from subprocess import call
+from subprocess import check_output
+import re
 
 
 def locgraph():
-    logs = call('git log --pretty=format:"%ai" --shortstat')
-    print(logs)
-    print('hi')
+    logs = check_output('git log --pretty=format:"%ai" --shortstat')
+    logs = logs.decode('utf-8')
+    commits = re.split(r'(.)\n\n', logs)
+    for commit in commits:
+        print('new')
+        print(commit)
+
