b5b250f17430a6de5d7cd343ff0123995d48f550
reustonium <andrew.ruestow@gmail.com>
2015-11-07 21:22:39 -0500

    Use string split instead of regex

--- gitgraph/graph.py
+++ gitgraph/graph.py
@@ -3,10 +3,10 @@ import re
 
 
 def locgraph():
-    logs = check_output('git log --pretty=format:"%ai" --shortstat')
-    logs = logs.decode('utf-8')
-    commits = re.split(r'(.)\n\n', logs)
+    logs = check_output('git log --pretty=format:"%ai" --shortstat').decode('utf-8')
+    #
+    commits = logs.split('\n\n')
     for commit in commits:
-        print('new')
-        print(commit)
+        date = re.match(r'(.*)', commit)
+        print(date.group(0)[:-5])
 
