c3cabe339809346f1da170f321a92a7b6a2dc112
reustonium <andrew.ruestow@gmail.com>
2015-11-08 19:08:45 -0500

    Build Data in foreach

--- gitgraph/graph.py
+++ gitgraph/graph.py
@@ -1,12 +1,20 @@
 from subprocess import check_output
 import re
-
+import bokeh
 
 def locgraph():
     logs = check_output('git log --pretty=format:"%ai" --shortstat').decode('utf-8')
-    #
+
+    dates=[]
+    data=[]
     commits = logs.split('\n\n')
     for commit in commits:
         date = re.match(r'(.*)', commit)
-        print(date.group(0)[:-5])
+        dates.append(date.group(0)[:-5])
+
+        additions = commit.split('\n')[1].split(' insertion')[0][-1:]
+        data.append(additions)
+
+    output_file("graph.html", title="LOC Graph")
+
 
